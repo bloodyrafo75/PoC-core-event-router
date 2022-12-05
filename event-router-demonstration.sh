f4e5abbcc2f9f6f224c1322f30b4b948565d6f34
rafael.carballo@travelgatex.com <rafael.carballo@travelgate.com>
2022-12-05 15:00:56 +0100

    Demonstration script

---
+++ event-router-demonstration.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/sh
+
+git clone https://github.com/bloodyrafo75/PoC-event-router-core-lib
+git clone https://github.com/bloodyrafo75/PoC-core-event-API
+git clone https://github.com/bloodyrafo75/PoC-core-event-router-CLI
+
+
+export GO111MODULE=on
+
+#Activate the API service.
+cd PoC-core-event-API/third_parties
+docker-compose up -d
+cd ..
+cp configs/.env.dist configs/.env
+go run cmd/main.go &
+echo "API server up"
+sleep 3
+
+#Activate the client.
+echo ""
+cd ../PoC-core-event-router-CLI
+cp configs/.env.dist configs/.env
+go run cmd/main.go & 
+echo "API Client up"
+sleep 3
+
+#Activate the 'sender'.
+echo ""
+echo "Sending example message"
+cd ../PoC-event-router-core-lib
+cp configs/.env.dist configs/.env
+go run cmd/main.go 
+
+#sleep 2 #waits for 2 seconds
+kill $(lsof -t -sTCP:LISTEN -i:3000)
